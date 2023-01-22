de7aca735d8a61a2c7293bb173b4c80170348c54
Stor4 <storchak850@gmail.com>
2023-01-22 12:21:18 +0200

    hw6

---
+++ hw6/scripts/scripts.js
@@ -0,0 +1,63 @@
+const arr1 = ['Vasya', 'Petya', 'Alexey']
+removeUser(arr1, 1)
+console.log(arr1) /// ['Vasya', 'Alexey']
+
+function removeUser (array, number) {
+    let a = array.splice(number,1)
+    console.log(a)
+}
+
+/////
+
+const obj0 = { name: 'Vasya', age: 1}
+getAllKeys(obj0) /// ["name", "age"]
+
+function getAllKeys (object) {
+    let tempObj = Object.keys(object)
+    console.log(tempObj)
+}
+
+/////
+
+const obj1 = { name: 'Vasya', age: 1}
+getAllValues(obj1) /// ["Vasya", 1]
+
+function getAllValues (object) {
+    let tempObj = Object.values(object)
+    console.log(tempObj)
+}
+
+/////
+
+const obj = {
+    id: 3,
+    name: 'Vasya'
+}
+
+const secondObj = {
+    id: 4,
+    name: 'Katya'
+}
+
+const arr = [
+    {
+        id: 1,
+        name: 'Kolya'
+    },
+    {
+        id: 2,
+        name: 'Petya'
+    },
+];
+
+insertIntoarr(obj, 2)
+console.log(arr) 
+/// [ {id: 1,name: 'Kolya'}, {id: 3, name: 'Vasya'}, {id: 2, name: 'Petya'} ]
+
+insertIntoarr(secondObj, 1)
+console.log(arr) 
+/// [ {id: 4,name: 'Katya'}, {id: 1,name: 'Kolya'}, {id: 3, name: 'Vasya'}, {id: 2, name: 'Petya'} ]
+
+function insertIntoarr (objectNumber, number) {
+    arr.splice(number-1, 0, objectNumber)
+}
